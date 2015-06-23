2a289352dd3ca042f5cd55581186153abb7c844d
haylin <dev@jqplex.com>
2015-06-23 17:45:26 +0800

    提交

---
+++ Entity/BaseEntities.cs
@@ -0,0 +1,90 @@
+﻿
+/************************************************************************ 
+ * 项目名称 :  Entity   
+ * 项目描述 :      
+ * 类 名 称 :  BaseEntities 
+ * 版 本 号 :  v1.0.0.0  
+ * 说    明 :      
+ * 作    者 :  dev 
+ * 创建时间 :  2015/6/23 10:49:24 
+ * 修 改 人 :  dev
+ * 修改说明 :
+ * 更新时间 :  2015/6/23 10:49:24 
+************************************************************************ 
+ * Copyright @ dev 2015. All rights reserved. 
+************************************************************************/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Entity
+{
+
+    /// <summary>
+    /// 实体基类
+    /// </summary>
+    public class BaseEntities
+    {
+        public BaseEntities()
+        {
+            //
+            //Todo:添加构造函数
+            //
+        }
+        public int Id { get; set; }
+        public override bool Equals(object obj)
+        {
+            return Equals(obj as BaseEntities);
+        }
+
+        private static bool IsTransient(BaseEntities obj)
+        {
+            return obj != null && Equals(obj.Id, default(int));
+        }
+
+        private Type GetUnproxiedType()
+        {
+            return GetType();
+        }
+
+        public virtual bool Equals(BaseEntities other)
+        {
+            if (other == null)
+                return false;
+
+            if (ReferenceEquals(this, other))
+                return true;
+
+            if (!IsTransient(this) &&
+                !IsTransient(other) &&
+                Equals(Id, other.Id))
+            {
+                var otherType = other.GetUnproxiedType();
+                var thisType = GetUnproxiedType();
+                return thisType.IsAssignableFrom(otherType) ||
+                        otherType.IsAssignableFrom(thisType);
+            }
+
+            return false;
+        }
+
+        public override int GetHashCode()
+        {
+            if (Equals(Id, default(int)))
+                return base.GetHashCode();
+            return Id.GetHashCode();
+        }
+
+        public static bool operator ==(BaseEntities x, BaseEntities y)
+        {
+            return Equals(x, y);
+        }
+
+        public static bool operator !=(BaseEntities x, BaseEntities y)
+        {
+            return !(x == y);
+        }
+    }
+}
---
+++ Service/DataBaseServices.cs
@@ -0,0 +1,66 @@
+﻿
+/************************************************************************ 
+ * 项目名称 :  Service   
+ * 项目描述 :      
+ * 类 名 称 :  DataBaseServices 
+ * 版 本 号 :  v1.0.0.0  
+ * 说    明 :      
+ * 作    者 :  dev 
+ * 创建时间 :  2015/6/23 9:40:45 
+ * 修 改 人 :  dev
+ * 修改说明 :
+ * 更新时间 :  2015/6/23 9:40:45 
+************************************************************************ 
+ * Copyright @ dev 2015. All rights reserved. 
+************************************************************************/
+
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.Common;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+
+namespace Service
+{
+    public class DataBaseServices
+    {
+        public DataBaseServices()
+        {
+            //
+            //Todo:添加构造函数
+            //
+        }
+
+        // 得到web.config里配置项的数据库连接字符串。  
+        private static readonly string ConnectionString =
+            ConfigurationManager.ConnectionStrings["ConnectionString"].ToString();
+        // 得到工厂提供器类型
+        private static readonly string ProviderFactoryString = ConfigurationManager.AppSettings["DBProvider"].ToString();
+
+        private static DbProviderFactory df = null;
+
+        /// <summary>  
+        /// 创建工厂提供器并且  
+        /// </summary> 
+        public static IDbConnection DbService()
+        {
+            if (df == null)
+                df = DbProviderFactories.GetFactory(ProviderFactoryString);
+            var connection = df.CreateConnection();
+            if (connection != null)
+            {
+                connection.ConnectionString = ConnectionString;
+                connection.Open();
+                return connection;
+            }
+            return null;
+        }
+
+       
+       
+    }
+
+}
