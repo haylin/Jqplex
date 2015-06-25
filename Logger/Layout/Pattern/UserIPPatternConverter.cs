fc0e0f54b853beffc657fbb0745a0e3799a6cd8e
haylin <dev@jqplex.com>
2015-06-25 22:43:06 +0800

    ok

---
+++ Logger/Layout/Pattern/UserIPPatternConverter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using log4net.Layout.Pattern;
+
+namespace Logger.Layout.Pattern
+{
+    internal class UserIPPatternConverter : PatternLayoutConverter
+    {
+        protected override void Convert(System.IO.TextWriter writer, log4net.Core.LoggingEvent loggingEvent)
+        {
+            if (loggingEvent.MessageObject is Logger.Extensions.CustomPatternMessage)
+            {
+                Logger.Extensions.CustomPatternMessage cpm = loggingEvent.MessageObject as Logger.Extensions.CustomPatternMessage;
+                writer.Write(cpm.UserIP);
+            }
+        }
+    }
+}
